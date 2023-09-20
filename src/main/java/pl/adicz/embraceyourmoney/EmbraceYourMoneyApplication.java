87afa69134db334687f9933cbdd8b56ceefce55e
Adrian Czyż <adicz5200@gmail.com>
2023-09-20 19:12:50 +0200

    Initial commit
    It all starts from here

---
+++ src/main/java/pl/adicz/embraceyourmoney/EmbraceYourMoneyApplication.java
@@ -0,0 +1,13 @@
+package pl.adicz.embraceyourmoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbraceYourMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmbraceYourMoneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/adicz/embraceyourmoney/EmbraceYourMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.adicz.embraceyourmoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmbraceYourMoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
